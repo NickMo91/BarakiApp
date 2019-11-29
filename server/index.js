ee142b1ec9bc80d1e4bf21ecbf304ed332ed1f82
NickMo <nikolasmoyseos@gmail.com>
2019-11-29 16:12:50 -0500

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// const parser = require('body-parser');
+// const Router = require("./routes")
+// const path = require("path")
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// app.use(parser.json());
+app.use(express.static(__dirname + './../dist'));
+// app.use("/", Router);
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname, './../dist/index.html'), function(err) {
+//     if (err) {
+//       res.status(500).send(err)
+//     }
+//   })
+// }) 
+// app.get('/', (req, res) => res.send('Hello G!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
